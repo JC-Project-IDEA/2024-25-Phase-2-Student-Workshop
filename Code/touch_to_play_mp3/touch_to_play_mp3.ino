05f195a716448e2a679d34bf1d5fe2867a25937c
賽馬會科藝共融計劃 <124440116+JC-Project-IDEA@users.noreply.github.com>
2025-05-28 10:02:39 +0800

    Update touch_to_play_mp3.ino

--- Code/touch_to_play_mp3/touch_to_play_mp3.ino
+++ Code/touch_to_play_mp3/touch_to_play_mp3.ino
@@ -41,6 +41,20 @@ void setup() {
     pinMode(i, INPUT);
   }
 
+  currentTouch0 = 1024 - analogRead(A0);
+  currentTouch1 = 1024 - analogRead(A1);
+  currentTouch2 = 1024 - analogRead(A2);
+  currentTouch3 = 1024 - analogRead(A3);
+  currentTouch4 = 1024 - analogRead(A4);
+  currentTouch5 = 1024 - analogRead(A5);
+
+  lastTouch0 = currentTouch0;
+  lastTouch1 = currentTouch1;
+  lastTouch2 = currentTouch2;
+  lastTouch3 = currentTouch3;
+  lastTouch4 = currentTouch4;
+  lastTouch5 = currentTouch5;
+
   TIMSK0 &= !(1 << TOIE0);
 }
 
@@ -55,21 +69,25 @@ void loop() {
 
   // compairing the changes by using if else statement
   if ((currentTouch0 - lastTouch0) >= TouchSensitivity) {
+    MP3player.stopTrack();
     MP3player.playTrack(0);
   } else if ((currentTouch1 - lastTouch1) >= TouchSensitivity) {
+    MP3player.stopTrack();
     MP3player.playTrack(1);
   } else if ((currentTouch2 - lastTouch2) >= TouchSensitivity) {
+    MP3player.stopTrack();
     MP3player.playTrack(2);
   } else if ((currentTouch3 - lastTouch3) >= TouchSensitivity) {
+    MP3player.stopTrack();
     MP3player.playTrack(3);
   } else if ((currentTouch4 - lastTouch4) >= TouchSensitivity) {
+    MP3player.stopTrack();
     MP3player.playTrack(4);
   } else if ((currentTouch5 - lastTouch5) >= TouchSensitivity) {
-    MP3player.playTrack(5);
-  } else {
     MP3player.stopTrack();
+    MP3player.playTrack(5);
   }
-
+  
   //print out the datas through serial monitor by the channel 115200(begin in the setup)
   Serial.print("currentTouch0: ");
   Serial.print(currentTouch0);
@@ -84,6 +102,6 @@ void loop() {
   lastTouch4 = currentTouch4;
   lastTouch5 = currentTouch5;
 
-  //delay 0.1s , 1000 = 1s 
+  //delay 0.1s , 1000 = 1s
   delay(100);
 }
